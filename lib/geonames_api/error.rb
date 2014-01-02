7e24b85d0cbf62c6d75dc55b69b1536f02d74074
Matthew McEachen <matthew+github@mceachen.org>
2014-01-02 13:56:13 -0800

    Support all error types from http://www.geonames.org/export/webservice-exception.html

--- lib/geonames_api/error.rb
+++ lib/geonames_api/error.rb
@@ -2,27 +2,51 @@ module GeoNamesAPI
   class Error < StandardError
     # See http://www.geonames.org/export/webservice-exception.html
     def self.from_status(status)
-      val = status['value'].to_i
-      error_type = case val
-        when 12, 13, 22
-          Timeout
-        when 14
-          InvalidParameter
-        when 21
-          InvalidInput
-        else
-          self
+      error_code = status['value'].to_i
+      error_class = for_error_code(error_code) || self
+      raise error_class, "#{status['message']} (#{error_code})"
+    end
+
+    def self.for_error_code(error_code)
+      @lookup ||= subclasses.reduce({}) do |h, subclass|
+        subclass::ERROR_CODES.each { |ea| h[ea] = subclass }
+        h
       end
-      raise error_type, "#{status['message']} (#{val})"
+      @lookup[error_code]
     end
   end
 
+  class AuthorizationException < Error
+    ERROR_CODES = [10]
+  end
+  class RecordDoesNotExist < Error
+    ERROR_CODES = [11]
+  end
   class Timeout < Error
+    ERROR_CODES = [13, 22]
   end
-
   class InvalidParameter < Error
+    ERROR_CODES = [14]
   end
-
-  class InvalidInput < Error
+  class NoResultFound < Error
+    ERROR_CODES = [15]
+  end
+  class DuplicateException < Error
+    ERROR_CODES = [16]
+  end
+  class PostalCodeNotFound < Error
+    ERROR_CODES = [17]
+  end
+  class DailyLimitExceeded < Error
+    ERROR_CODES = [18]
+  end
+  class HourlyLimitExceeded < Error
+    ERROR_CODES = [19]
+  end
+  class WeeklyLimitExceeded < Error
+    ERROR_CODES = [20]
+  end
+  class ServiceNotImplemented < Error
+    ERROR_CODES = [23]
   end
 end
--- spec/geonames_api/retry_spec.rb
+++ spec/geonames_api/retry_spec.rb
@@ -1,27 +1,25 @@
 require 'spec_helper'
 
 describe GeoNamesAPI::Base do
-  TIMEOUT = JSON.load <<-JSON
-    {
-      "status": {
-        "message": "ERROR: canceling statement due to statement timeout",
-        "value": 13
-      }
-    }
-  JSON
-
-  # This is not a correct, complete response, but that's not relevant to the spec:
-  EXAMPLE_RESPONSE = JSON.load <<-JSON
-    {"geonames": [{ "name": "Earth" }]}
-  JSON
-
-  describe "::find" do
+  describe "retries" do
     before :each do
       GeoNamesAPI.max_sleep_time_between_retries = 0
+      @timeout = JSON.load <<-JSON
+        {
+          "status": {
+            "message": "ERROR: canceling statement due to statement timeout",
+            "value": 13
+          }
+        }
+      JSON
+      # This is not a correct, complete response, but that's not relevant to the spec:
+      @response = JSON.load <<-JSON
+        {"geonames": [{ "name": "Earth" }]}
+      JSON
     end
 
     it "retries when geonames returns timeout errors" do
-      GeoNamesAPI::Hierarchy.stub(:make_request).and_return(TIMEOUT, EXAMPLE_RESPONSE)
+      GeoNamesAPI::Hierarchy.stub(:make_request).and_return(@timeout, @response)
       hierarchy = GeoNamesAPI::Hierarchy.find(6295630)
       earth = hierarchy.first
       earth.should be_present
@@ -29,7 +27,7 @@ describe GeoNamesAPI::Base do
     end
 
     it "fails when geonames returns timeout errors too many times" do
-      GeoNamesAPI::Hierarchy.stub(:make_request).and_return(TIMEOUT, TIMEOUT, EXAMPLE_RESPONSE)
+      GeoNamesAPI::Hierarchy.stub(:make_request).and_return(@timeout, @timeout, @response)
       GeoNamesAPI.retries = 1
       proc { GeoNamesAPI::Hierarchy.find(6295630) }.should raise_error GeoNamesAPI::Timeout
     end
