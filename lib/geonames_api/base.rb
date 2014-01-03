96d11c3d8d1abcd6ff2327b9cc9b97e200843674
Matthew McEachen <matthew+github@mceachen.org>
2014-01-03 09:52:56 -0800

    "Cannot get a connection, pool error Timeout waiting for idle object" is error code 13, and should be categorized as a Timeout

--- lib/geonames_api/base.rb
+++ lib/geonames_api/base.rb
@@ -28,7 +28,9 @@ module GeoNamesAPI
       rescue Timeout => e
         if retries_remaining > 0
           retries_remaining -= 1
-          sleep rand * GeoNamesAPI.max_sleep_time_between_retries
+          sleep_time = rand * GeoNamesAPI.max_sleep_time_between_retries
+          GeoNamesAPI.logger.info("Retrying in #{sleep_time}s (#{e})") if GeoNamesAPI.logger
+          sleep sleep_time
           retry
         else
           raise e
