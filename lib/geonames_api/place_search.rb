1103dce1653a4bf8eac9c5af2d99f3036d8cc65a
Vitalie D <vitalied@users.noreply.github.com>
2014-04-16 16:53:42 +0300

    Update place_search.rb

--- lib/geonames_api/place_search.rb
+++ lib/geonames_api/place_search.rb
@@ -4,6 +4,8 @@ module GeoNamesAPI
     METHOD = 'searchJSON'
     FIND_PARAMS = %w(q maxRows)
     DEFAULT_MAX_ROWS = 100
+    
+    attr_reader :response
 
     def self.find_by_place_name(name, max_rows = DEFAULT_MAX_ROWS)
       where(name: name, maxRows: max_rows)
